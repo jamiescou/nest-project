22c2d705e2ac937f97673ed08d825182ab920e86
chenmy <895849097@qq.com>
2024-03-20 15:20:18 +0800

    修改流量卡接口

--- src/card/card.controller.ts
+++ src/card/card.controller.ts
@@ -10,7 +10,7 @@ import * as fs from 'fs';
 import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
 import { CardService } from './card.service';
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
-import { CreateCardDto, CardRo } from './dto/card.dto';
+import { CreateCardDto, CardRo, CardInfoDto } from './dto/card.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard, Roles } from '../auth/role.guard';
 import { fstat } from 'fs';
@@ -37,47 +37,58 @@ export class CardController {
    * 获取指定文章
    * @param id
    */
-  @ApiOperation({ summary: '获取指定流量卡' })
+  @ApiOperation({ summary: '导入所有流量卡' })
   // @ApiBearerAuth()
   // @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Get('/import')
-  async importAllCard(): Promise<boolean> {
-    const data = fs.readFileSync('src/card/dto/card.json', 'utf8');
-    const cards = JSON.parse(data);
-    console.log('cards', cards);
-    const results = cards.data.map((item) => {
-      return {
-        id: item.id,
-        productID: item.productID,
-        productName: item.productName,
-        remark: item.remark,
-        mainPic: item.mainPic,
-        littlepicture: item.littlepicture,
-        userID: item.userID,
-        rule: item.rule,
-        areaRead: item.areaRead,
-        acquireModel: item.acquireModel,
-        idCardRepeat: item.idCardRepeat,
-        phoneRepeat: item.phoneRepeat,
-        firstLevelMoney: item.firstLevelMoney,
-        numberSel: item.numberSel,
-        backMoneyType: item.backMoneyType,
-        age2: item.age2,
-        age1: item.age1,
-        zhutui: item.zhutui,
-        disableArea: item.disableArea,
-        area: item.area,
-        photoType: item.photoType,
-        positionform: item.positionform,
-        parentProductID: item.parentProductID,
-        operator: item.operator,
-        sPriceRead: item.sPriceRead,
-        sPrice: item.sPrice,
-        commissionType: item.commissionType,
-        count: null,
-      };
-    });
-    await this.cardService.importCard(results);
-    return true;
+  async importAllCard(@Query('fileName') fileName: string): Promise<any> {
+    try {
+      const data = fs.readFileSync(
+        'public/files/' + fileName + '.json',
+        'utf8',
+      );
+      const cards = JSON.parse(data);
+      const results = cards.data.map((item) => {
+        return {
+          id: item.id,
+          productID: item.productID,
+          productName: item.productName,
+          remark: item.remark,
+          mainPic: item.mainPic,
+          littlepicture: item.littlepicture,
+          userID: item.userID,
+          rule: item.rule,
+          areaRead: item.areaRead,
+          acquireModel: item.acquireModel,
+          idCardRepeat: item.idCardRepeat,
+          phoneRepeat: item.phoneRepeat,
+          firstLevelMoney: item.firstLevelMoney,
+          numberSel: item.numberSel,
+          backMoneyType: item.backMoneyType,
+          age2: item.age2,
+          age1: item.age1,
+          zhutui: item.zhutui,
+          disableArea: item.disableArea,
+          area: item.area,
+          photoType: item.photoType,
+          positionform: item.positionform,
+          parentProductID: item.parentProductID,
+          operator: item.operator,
+          sPriceRead: item.sPriceRead,
+          sPrice: item.sPrice,
+          commissionType: item.commissionType,
+          count: null,
+        };
+      });
+      await this.cardService.importCard(results);
+      return '数据导入成功';
+    } catch (error) {
+      return '数据文件不存在';
+    }
+  }
+  @ApiOperation({ summary: '根据ID查找流量卡' })
+  @Get('/findCardById')
+  async findCardById(@Query('id') id: string): Promise<CardInfoDto> {
+    return await this.cardService.findById(id);
   }
 }
