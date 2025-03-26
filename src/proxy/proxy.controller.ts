e901006cb78557e1c49dec57e7e9e5c69c5da9fa
chenmy <895849097@qq.com>
2025-03-26 10:03:11 +0800

    feat: 增加机器人超时时间

--- src/proxy/proxy.controller.ts
+++ src/proxy/proxy.controller.ts
@@ -2,12 +2,14 @@
  * @Author: chenmy 895849097@qq.com
  * @Date: 2024-06-25 11:21:41
  * @LastEditors: chenmy 895849097@qq.com
- * @LastEditTime: 2025-03-04 15:47:05
+ * @LastEditTime: 2025-03-26 10:01:45
  * @FilePath: /nest-blog-main/src/proxy/proxy.controller.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { Controller, Post, Query, Res, Body, Headers } from '@nestjs/common';
+import { Controller, Post, Query, Get, Body, Headers } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { ProxyService } from './proxy.service';
+import * as fs from 'fs';
 
 interface ChatMessage {
   model: string;
@@ -29,5 +31,25 @@ export class ProxyController {
   getChatMessage(@Body() body: ChatMessage, @Headers() header) {
     console.log('header==>', body, header);
     return this.proxyService.getChatMessage(body);
+  }
+
+  @Get('/setRobotDate')
+  @ApiOperation({ summary: '获取机器人定时时间' })
+  async setRobotDate(@Query('time') time?: string) {
+    console.log('time==>', time);
+    // nestjs 读写文件txt
+    const data = fs.writeFileSync('./public/robot.txt', time, 'utf-8');
+    console.log('data==>', data);
+    return {
+      time: data,
+    };
+  }
+  @Get('/getRobotDate')
+  @ApiOperation({ summary: '获取机器人定时时间' })
+  async getRobotDate() {
+    const data = fs.readFileSync('./public/robot.txt', 'utf-8');
+    return {
+      time: data,
+    };
   }
 }
